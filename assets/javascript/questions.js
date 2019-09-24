6881f33a60f8c262986609ed848e6399e73e5b11
thaislip <taber.haislip@gmail.com>
2019-09-24 15:03:08 -0400

    functionality in .app and .questions complete; background images add/style added to css.

--- assets/javascript/questions.js
+++ assets/javascript/questions.js
@@ -1,7 +1,7 @@
 const quizQuestions = [
     {
 
-    question:'q1 What year did Star Wars: a New Hope (episode IV) release in theaters?',
+    question:'What year did Star Wars: a New Hope (episode IV) release in theaters?',
     choices: ["1977","1972","1984","1965"],
     correctAnswer:"1977"
     
@@ -9,33 +9,33 @@ const quizQuestions = [
 
     {
 
-    question:'q2 What year did Star Wars: a New Hope (episode IV) release in theaters?',
-    choices: ["1977","1972","1984","1965"],
-    correctAnswer:"1977"
+    question:"what color is Yoda's light-saber?",
+    choices: ["Red","Blue","Green","Purple"],
+    correctAnswer:"Green"
         
     },
     
     {
 
-    question:'q3 What year did Star Wars: a New Hope (episode IV) release in theaters?',
-    choices: ["1977","1972","1984","1965"],
-    correctAnswer:"1965"
+    question:'What creature captures Luke on the planet Hoth?',
+    choices: ["Wompa","Womp Rat","Rodian","Bantha"],
+    correctAnswer:"Wompa"
             
     },
 
     {
 
-    question:'q4 What year did Star Wars: a New Hope (episode IV) release in theaters?',
-    choices: ["1977","1972","1984","1965"],
-    correctAnswer:"1972"
+    question:'What main character is related to Luke Skywalker?',
+    choices: ["Han Solo","Obi-Wan","Princess Leia","Jar Jar Binks"],
+    correctAnswer:"Princess Leia"
         
     },
 
     {
 
-    question:'q5 What year did Star Wars: a New Hope (episode IV) release in theaters?',
-    choices: ["1977","1972","1984","1965"],
-    correctAnswer:"1984"
+    question:'Who wrote and directed Star Wars?',
+    choices: ["Tim Burton","George Foreman","Steven Spielberg","George Lucas"],
+    correctAnswer:"George Lucas"
         
     },
         
